c756566831066c5270fa77740f298e4f499aebfe
Rohan Kasodariya <rohankasodariya@Rohans-MacBook-Air.local>
2025-07-13 16:54:38 -0400

    Updated folder structer added

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s')
+logging.basicConfig(level=logging.INFO, format= '[%(asctime)s]: %(message)s')
 
 project_name = "WineQualityPrediction"
 
@@ -13,11 +13,12 @@ list_of_files = [
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml"
+    "config/config.yaml",
     "params.yaml",
     "schema.yaml",
     "main.py",
@@ -25,21 +26,22 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir, filename = os.path.split(filepath)
+    filedir,filename = os.path.split(filepath)
 
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for the file: {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
     
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass
-        logging.info(f"Creating empty file:{filepath}")
+            logging.info(f"Creating empty file: {filepath}")
     
     else:
         logging.info(f"{filename} is already exists")
+
